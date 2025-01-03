23c92120d8e8492c4132ec1ca915db1cdbfb7cac
ellen-firs <148352828+ellen-firs@users.noreply.github.com>
2025-01-04 00:25:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,89 +25,86 @@ from yandex_music import Client
 
 uploaded_file = st.file_uploader("Загрузите файл history.json", type=["json"])
 if uploaded_file:
-    file_name = uploaded_file.name
+    try:
+        history = json.load(uploaded_file)  # Загружаем JSON-объект из загруженного файла
 
-    start_time = datetime.fromisoformat(START_TIME)
-    end_time = datetime.fromisoformat(END_TIME)
-    
-    filtered = []
-    
 
-    history = json.load(file_name)
-    for item in history:
-        item_time = datetime.fromisoformat(item["timestamp"].rstrip("Z") + "+00:00")
-        if start_time <= item_time < end_time:
-            filtered.append(item)
-    
-    client = Client().init()
-    rich_results = client.tracks(list(set(list(map(lambda x: x['id'], filtered)))))
-    ids_map = {x.id: x for x in rich_results}
-    
-    scrobbles = []
-    problems = []
-    
-    for item in filtered:
-        try:
-            track_info = {"artist": ids_map[item['id']].artists[0].name,
-                          "title": ids_map[item['id']].title,
-                          "album": ids_map[item['id']].albums[0].title if ids_map[item['id']].albums else None,
-                          "timestamp": item['timestamp'],
-                          "duration": ids_map[item['id']].duration_ms // 1000
-                          if ids_map[item['id']].duration_ms is not None
-                          else None,
-                          }
-            print(list(track_info.values()))
-            scrobbles.append(track_info)
-        except:
-            problems.append(item)
-    
-    if len(problems):
-        with open("problems_tracks.txt", "w") as f:
-            for item in problems:
-                print(item, ids_map[item['id']].artists[0].name)
-    
-    print("Проверьте начало и конец списка")
-    ans = input("Загрузить в Last.fm? [yes/No]\n")
-    
-    if ans[0] == "y":
-        import os
-        import pylast
-    
-        SESSION_KEY_FILE = ".session_key"
-        network = pylast.LastFMNetwork("15ea55ecc2d12d5910bd1a56c89fb604",
-                                       "2dff873c07e12a590e512c62b86b899c")
-        if not os.path.exists(SESSION_KEY_FILE):
-            skg = pylast.SessionKeyGenerator(network)
-            url = skg.get_web_auth_url()
-    
-            print(f"Пожалуйста, авторизуйтесь в last.fm: {url}\n")
-            import time
-            import webbrowser
-    
-            webbrowser.open(url)
-    
-            while True:
-                try:
-                    session_key = skg.get_web_auth_session_key(url)
-                    with open(SESSION_KEY_FILE, "w") as f:
-                        f.write(session_key)
-                    break
-                except pylast.WSError:
-                    time.sleep(1)
-        else:
-            session_key = open(SESSION_KEY_FILE).read()
-    
-        network.session_key = session_key
-    
-        print("Начало загрузки")
-        for track in scrobbles:
-            print("Загружаем ", track, end="")
-            timestamp = int(datetime.fromisoformat(track["timestamp"].rstrip("Z") + "+00:00").timestamp())
-            lastfm_user = network.get_user(network.username)
-            network.scrobble(artist=track["artist"],
-                             title=track["title"],
-                             album=track['album'],
-                             duration=track['duration'],
-                             timestamp=timestamp)
-            print(" Done!")
-        print("Всё загружено")
+        start_time = datetime.fromisoformat(START_TIME)
+        end_time = datetime.fromisoformat(END_TIME)
+        for item in history:
+            item_time = datetime.fromisoformat(item["timestamp"].rstrip("Z") + "+00:00")
+            if start_time <= item_time < end_time:
+                filtered.append(item)
+        
+        client = Client().init()
+        rich_results = client.tracks(list(set(list(map(lambda x: x['id'], filtered)))))
+        ids_map = {x.id: x for x in rich_results}
+        
+        scrobbles = []
+        problems = []
+        
+        for item in filtered:
+            try:
+                track_info = {"artist": ids_map[item['id']].artists[0].name,
+                              "title": ids_map[item['id']].title,
+                              "album": ids_map[item['id']].albums[0].title if ids_map[item['id']].albums else None,
+                              "timestamp": item['timestamp'],
+                              "duration": ids_map[item['id']].duration_ms // 1000
+                              if ids_map[item['id']].duration_ms is not None
+                              else None,
+                              }
+                print(list(track_info.values()))
+                scrobbles.append(track_info)
+            except:
+                problems.append(item)
+        
+        if len(problems):
+            with open("problems_tracks.txt", "w") as f:
+                for item in problems:
+                    print(item, ids_map[item['id']].artists[0].name)
+        
+        print("Проверьте начало и конец списка")
+        ans = input("Загрузить в Last.fm? [yes/No]\n")
+        
+        if ans[0] == "y":
+            import os
+            import pylast
+        
+            SESSION_KEY_FILE = ".session_key"
+            network = pylast.LastFMNetwork("15ea55ecc2d12d5910bd1a56c89fb604",
+                                           "2dff873c07e12a590e512c62b86b899c")
+            if not os.path.exists(SESSION_KEY_FILE):
+                skg = pylast.SessionKeyGenerator(network)
+                url = skg.get_web_auth_url()
+        
+                print(f"Пожалуйста, авторизуйтесь в last.fm: {url}\n")
+                import time
+                import webbrowser
+        
+                webbrowser.open(url)
+        
+                while True:
+                    try:
+                        session_key = skg.get_web_auth_session_key(url)
+                        with open(SESSION_KEY_FILE, "w") as f:
+                            f.write(session_key)
+                        break
+                    except pylast.WSError:
+                        time.sleep(1)
+            else:
+                session_key = open(SESSION_KEY_FILE).read()
+        
+            network.session_key = session_key
+        
+            print("Начало загрузки")
+            for track in scrobbles:
+                print("Загружаем ", track, end="")
+                timestamp = int(datetime.fromisoformat(track["timestamp"].rstrip("Z") + "+00:00").timestamp())
+                lastfm_user = network.get_user(network.username)
+                network.scrobble(artist=track["artist"],
+                                 title=track["title"],
+                                 album=track['album'],
+                                 duration=track['duration'],
+                                 timestamp=timestamp)
+                print(" Done!")
+            print("Всё загружено")
