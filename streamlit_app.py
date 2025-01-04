99494234807b103e45e2b965597c032947c3e10d
ellen-firs <148352828+ellen-firs@users.noreply.github.com>
2025-01-04 03:22:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@
 
 import streamlit as st
 from datetime import datetime, time
-start_date = st.date_input("Выберите начальную дату")
-start_time = st.time_input("Выберите начальное время")
-START_TIME = datetime.combine(start_date, start_time)
+start_date_choose = st.date_input("Выберите начальную дату")
+start_time_choose = st.time_input("Выберите начальное время")
+START_TIME = datetime.combine(start_date_choose, start_time_choose)
 
-end_date = st.date_input("Выберите конечную дату")
-end_time = st.time_input("Выберите конечное время")
-END_TIME = datetime.combine(end_date, end_time)
+end_date_choose = st.date_input("Выберите конечную дату")
+end_time_choose = st.time_input("Выберите конечное время")
+END_TIME = datetime.combine(end_date_choose, end_time_choose)
 
 
 filtered = []
