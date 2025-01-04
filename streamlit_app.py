bca83497be770bbbe05f8a98ba86e3ac0c15caf8
ellen-firs <148352828+ellen-firs@users.noreply.github.com>
2025-01-04 17:05:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,10 +78,10 @@ if uploaded_file:
                      print(item, ids_map[item['id']].artists[0].name)
         
         st.write("Проверьте начало и конец списка")
-        df = pd.DataFrame(scrobbles)
-        print(df)
-        df_reversed = df.iloc[::-1]
-        st.dataframe(df_reversed) 
+        df = pd.DataFrame(list(track_info.values())
+        st.dataframe(df)
+        #df_reversed = df.iloc[::-1]
+        #st.dataframe(df_reversed) 
         st.write("Загрузить в Last.fm? Введите 'yes' или 'no'.")
 
         ans = st.text_input("Ваш ответ:")
