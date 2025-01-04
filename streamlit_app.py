a25cc1e182780b045e3240ad4bafb91ca1fb043e
ellen-firs <148352828+ellen-firs@users.noreply.github.com>
2025-01-04 16:45:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -129,7 +129,7 @@ if uploaded_file:
             
             for track in scrobbles:
                 #st.write("Загружаем ", *track.values(), end="")
-                content += f"<p>Загружаем: {track_info}    ✅</p>"
+                content = f"<p>Загружаем: {track_info}    ✅</p>"
                 scrollable_container.markdown(
                     scrollable_html.format(content=content), unsafe_allow_html=True
                 )
